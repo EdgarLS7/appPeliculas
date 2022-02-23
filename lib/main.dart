68ff7b486ca02c039a45e26f4fa3fc2ec2f6bcd6
Juan Gonzalez <l17160013@eldorado.tecnm.mx>
2022-02-23 16:54:41 -0700

    Implementacion de las TarjetasÃ

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'package:flt_peliculas/screens/screens.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,11 +14,11 @@ class MyApp extends StatelessWidget {
       title: 'Peliculas',
       initialRoute: '/home',
       routes: {
-        '/home'   : ( _ ) => HomeScreen(),
-        '/details': ( _ ) => DetailsScreen(),
+        '/home'   : ( _ ) => const HomeScreen(),
+        '/details': ( _ ) => const DetailsScreen(),
       },
       theme: ThemeData.light().copyWith(
-        appBarTheme: AppBarTheme(
+        appBarTheme: const AppBarTheme(
           color: Colors.red
         )
       )
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flt_peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Peliculas en cines'),
+        title: const Text('Peliculas en cines'),
         elevation: 0,
         actions: [
           IconButton(
-            icon: Icon(Icons.search_outlined),
+            icon: const Icon(Icons.search_outlined),
             onPressed: () {},
           )
         ],
