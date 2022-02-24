95ce84dc61fe9d6c6a6dd2ebccaff35d28ac6cb6
Edgar Lopez <16171409@itculiacan.edu.mx>
2022-02-24 13:52:11 -0700

    Sliver AppBar

--- lib/screens/details.screen.dart
+++ lib/screens/details.screen.dart
@@ -5,10 +5,42 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //TODO: Cambiar luego por una instancia de movie
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'No Movie';
+
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      )
+    );
+  }
+}
+
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 180,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 12),
+          ),
         ),
       ),
     );
