6ca43f61501765f117ba318334e7e03265c0d81e
Edgar Lopez <16171409@itculiacan.edu.mx>
2022-02-24 14:29:05 -0700

    Poster, Titulos y subtitulos de las peliculas

--- lib/screens/details.screen.dart
+++ lib/screens/details.screen.dart
@@ -12,13 +12,17 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle()
+            ])
+          )
         ],
       )
     );
   }
 }
-
 
 class _CustomAppBar extends StatelessWidget {
   const _CustomAppBar({Key? key}) : super(key: key);
@@ -43,6 +47,60 @@ class _CustomAppBar extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://via.placeholder.com/150x200'),
+              height: 150,
+            ),
+          ),
+
+          SizedBox(width: 20,),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title', 
+                style: textTheme.headline5, 
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,),
+              Text(
+                'movie.originalTitle',
+                style: textTheme.subtitle2,
+                overflow: TextOverflow.ellipsis,
+              ),
+
+              Row(
+                children: [
+                  Icon(Icons.star_border_outlined, size: 15, color: Colors.grey,),
+                  SizedBox(width: 4,),
+                  Text('Movie.voteAvergae', style: textTheme.caption)
+                ],
+              )
+
+            ],
+          )
+        ],
+      )
     );
   }
 }
