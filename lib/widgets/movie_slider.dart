4720b33b138ceca227475a746f47892aff251412
Edgar Lopez <16171409@itculiacan.edu.mx>
2022-02-28 13:18:54 -0700

    Scroll Infinito

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -1,16 +1,43 @@
 import 'package:flt_peliculas/models/models.dart';
 import 'package:flutter/material.dart';
 
-class MovieSlider extends StatelessWidget {
+class MovieSlider extends StatefulWidget {
 
   final List <Movie> movies;
   final String? title;
+  final Function onNextPage;
 
   const MovieSlider({
     Key? key, 
     required this.movies, 
-    this.title
+    this.title, 
+    required this.onNextPage
   }) : super(key: key);
+
+  @override
+  State<MovieSlider> createState() => _MovieSliderState();
+}
+
+class _MovieSliderState extends State<MovieSlider> {
+
+  final ScrollController scrollController = new ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() { 
+      if (scrollController.position.pixels >= scrollController.position.maxScrollExtent - 500) {
+        widget.onNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,19 +48,21 @@ class MovieSlider extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
 
-          if (this.title != null)
+          if (this.widget.title != null)
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Text( this.title!, style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold),)
+            child: Text( this.widget.title!, style: TextStyle( fontSize: 15, fontWeight: FontWeight.bold),)
           ),
           
           const SizedBox(height: 3,),
 
+          //Slide lateral
           Expanded(
             child: ListView.builder(
+              controller: scrollController,
               scrollDirection: Axis.horizontal,
-              itemCount: movies.length,
-              itemBuilder: ( _ , int index) => _MoviePoster(movies[index])
+              itemCount: widget.movies.length,
+              itemBuilder: ( _ , int index) => _MoviePoster(widget.movies[index])
             ),
           ),
         ],
